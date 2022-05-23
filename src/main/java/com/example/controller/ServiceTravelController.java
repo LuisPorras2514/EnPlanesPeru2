eaf1d29e9234b97ff64b32ec603a03a82066462f
Luis Alfredo Porras Tarifeño <69887242+LuisPorras2514@users.noreply.github.com>
2022-05-23 17:05:30 -0500

    CRUD Servicios Finalizado

--- src/main/java/com/example/controller/ServiceTravelController.java
+++ src/main/java/com/example/controller/ServiceTravelController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.entities.ServiceTravel;
@@ -18,7 +19,7 @@ import com.example.service.ServiceTypeService;
 public class ServiceTravelController {
 	private ServiceTravelService serviceTravelService;
 	private ServiceTypeService serviceTypeService;
-	
+
 	private List<ServiceType> serviceTypes = new ArrayList<>();
 
 	public ServiceTravelController(ServiceTravelService serviceTravelService, ServiceTypeService serviceTypeService) {
@@ -36,17 +37,52 @@ public class ServiceTravelController {
 	public String createStudentForm(Model model) {
 		ServiceTravel serviceTravel = new ServiceTravel();
 		serviceTypes = serviceTypeService.getAllServiceTypes();
-		
-		model.addAttribute("serviceTravel",serviceTravel);
-		model.addAttribute("serviceTypes",serviceTypes);
-		
+
+		model.addAttribute("serviceTravel", serviceTravel);
+		model.addAttribute("serviceTypes", serviceTypes);
+
 		return "insert-service";
 	}
-	
+
 	@PostMapping("/services")
-	public String saveStudent(@ModelAttribute("serviceTravel") ServiceTravel serviceTravel){
+	public String saveStudent(@ModelAttribute("serviceTravel") ServiceTravel serviceTravel) {
 		serviceTravel.setStar(0);
 		serviceTravelService.saveServiceTravel(serviceTravel);
 		return "redirect:/";
 	}
+
+	@GetMapping("/services/edit/{id}")
+	public String editServicesTravelForm(@PathVariable Long id, Model model) {
+		ServiceTravel serviceTravel = serviceTravelService.getServiceTravelById(id);
+
+		model.addAttribute("serviceTravel", serviceTravel);
+		model.addAttribute("serviceTypes", serviceTypes);
+
+		return "edit-service";
+	}
+
+	@PostMapping("/services/{id}")
+	public String updateServicesTravel(@PathVariable Long id, @ModelAttribute("student") ServiceTravel serviceTravel,
+			Model model) {
+
+		ServiceTravel existentserviceTravel = serviceTravelService.getServiceTravelById(id);
+
+		existentserviceTravel.setId(id);
+		existentserviceTravel.setName(serviceTravel.getName());
+		existentserviceTravel.setDeparment(serviceTravel.getDeparment());
+		existentserviceTravel.setProvince(serviceTravel.getProvince());
+		existentserviceTravel.setAddress(serviceTravel.getAddress());
+		existentserviceTravel.setStar(serviceTravel.getStar());
+
+		serviceTravelService.updateServiceTravel(existentserviceTravel);
+
+		return "redirect:/";
+	}
+
+	@GetMapping("/services/{id}")
+	public String deleteServiceTravel(@PathVariable Long id) {
+		serviceTravelService.deleteServiceTravel(id);
+		return "redirect:/";
+	}
+
 }
